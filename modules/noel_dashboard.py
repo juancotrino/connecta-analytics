7e9d7d3c8843f3a7dde010967256ce1cbb766ab4
Juan Cotrino <juan.cotrino@outlook.com>
2024-05-21 11:59:25 -0500

    Added dashboard for norma

---
+++ modules/noel_dashboard.py
@@ -0,0 +1,105 @@
+from io import BytesIO
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+@st.cache_data
+def get_data():
+
+    data = pd.read_excel('static/db/norma_noel.xlsx')
+
+    transformed_data = data.melt(
+        id_vars=data.columns[:16],
+        value_vars=data.columns[16:]
+    ).dropna(subset='value').reset_index(drop=True)
+
+    transformed_data['Categoría'] = transformed_data['Categoría'].apply(lambda x: x.strip())
+    transformed_data['sample'] = transformed_data['sample'].astype(str)
+    transformed_data['Género'] = transformed_data['Género'].astype(str)
+    transformed_data['Edad'] = transformed_data['Edad'].apply(lambda x: x if isinstance(x, (float, int)) else np.nan)
+    transformed_data['Estrato/Nivel socieconómico'] = transformed_data['Estrato/Nivel socieconómico'].astype(str)
+    transformed_data['País'] = transformed_data['País'].apply(lambda x: x.capitalize())
+
+    return transformed_data
+
+# Function to convert DataFrame to Excel and return as bytes
+def to_excel_bytes(df):
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=True, sheet_name='Sheet1')
+    return output.getvalue()
+
+def calculate_statistics_jr_scale(data: pd.DataFrame, query: str | None = None):
+
+    if query:
+        data = data.query(query)
+
+    # data.query("`Número del estudio` == 6428")
+
+    statistics_data = pd.DataFrame()
+
+    indexed_data = data.set_index(data.columns[:-2].to_list())
+    indexed_data = indexed_data[indexed_data['value'].apply(lambda x: isinstance(x, float))]
+    indexed_data['value'] = indexed_data['value'].astype(int)
+
+    indexed_data = indexed_data[indexed_data['variable'].str.contains('JR')]
+
+    statistics_data['base'] = indexed_data.groupby(['variable']).count()
+    statistics_data['mean'] = indexed_data.groupby(['variable']).mean()
+    statistics_data['std'] = indexed_data.groupby(['variable']).std()
+
+    # JR
+    statistics_data = pd.merge(
+        statistics_data,
+        indexed_data[indexed_data['value'] == 3].rename(columns={'value': 'JR'}).groupby(['variable']).count(),
+        left_index=True,
+        right_index=True
+    )
+    statistics_data['%JR'] = statistics_data['JR'] / statistics_data['base']
+
+    return statistics_data
+
+def calculate_statistics_regular_scale(data: pd.DataFrame, query: str | None = None):
+
+    if query:
+        data = data.query(query)
+
+    # data.query("`Número del estudio` == 6428")
+
+    statistics_data = pd.DataFrame()
+
+    indexed_data = data.set_index(data.columns[:-2].to_list())
+    indexed_data = indexed_data[indexed_data['value'].apply(lambda x: isinstance(x, (float, int)))]
+    indexed_data['value'] = indexed_data['value'].astype(int)
+
+    indexed_data = indexed_data[~indexed_data['variable'].str.contains('JR')]
+
+    statistics_data['base'] = indexed_data.groupby(['variable']).count()
+    statistics_data['mean'] = indexed_data.groupby(['variable']).mean()
+    statistics_data['std'] = indexed_data.groupby(['variable']).std()
+
+    statistics_data = pd.merge(
+        statistics_data,
+        indexed_data[indexed_data['value'] == 5].rename(columns={'value': 'TB'}).groupby(['variable']).count(),
+        left_index=True,
+        right_index=True
+    )
+    statistics_data = pd.merge(
+        statistics_data,
+        indexed_data[indexed_data['value'].isin([4, 5])].rename(columns={'value': 'T2B'}).groupby(['variable']).count(),
+        left_index=True,
+        right_index=True
+    )
+    statistics_data = pd.merge(
+        statistics_data,
+        indexed_data[indexed_data['value'].isin([1, 2])].rename(columns={'value': 'B2B'}).groupby(['variable']).count(),
+        left_index=True,
+        right_index=True
+    )
+
+    statistics_data['%TB'] = statistics_data['TB'] / statistics_data['base']
+    statistics_data['%T2B'] = statistics_data['T2B'] / statistics_data['base']
+    statistics_data['%B2B'] = statistics_data['B2B'] / statistics_data['base']
+
+    return statistics_data
---
+++ pages/03_NOEL_Dashboard.py
@@ -0,0 +1,133 @@
+
+import itertools
+from PIL import Image
+import streamlit as st
+
+from streamlit_option_menu import option_menu
+
+from modules.styling import apply_default_style
+from modules.noel_dashboard import (
+    get_data,
+    to_excel_bytes,
+    calculate_statistics_regular_scale,
+    calculate_statistics_jr_scale
+)
+
+# -------------- SETTINGS --------------
+page_title = "Noel Dashboard"
+page_icon = Image.open('static/images/connecta-logo.png')  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+
+# --------------------------------------
+
+apply_default_style(
+    page_title,
+    page_icon,
+    initial_sidebar_state='expanded'
+)
+
+st.sidebar.markdown("# Noel Dashboard")
+st.sidebar.markdown("""
+This is a dashboard where TP, B2B, JR and other metrics can be
+seen in a compact and easy to filter and understandlable way.
+""")
+
+st.title(page_title)
+st.header('Filters')
+
+data = get_data()
+
+col11, col12 = st.columns(2)
+col21, col22, col23, col24 = st.columns(4)
+col31, col32, col33, col34 = st.columns(4)
+
+filters_template = {
+    'Categoría': ('Category', col11),
+    'Sub - Categoría': ('Sub Category', col12),
+    'Cliente': ('Client', col21),
+    'Nombre del estudio': ('Study Name', col22),
+    'Marca': ('Brand', col23),
+    'sample': ('Sample', col24),
+    'Edad': ('Age', col31),
+    'Género': ('Gender', col32),
+    'Estrato/Nivel socieconómico': ('SES', col33),
+    'País': ('Country', col34),
+}
+
+filters = {filter_name: [] for filter_name in filters_template.keys()}
+
+for filter_name, attributes in filters_template.items():
+    name, field = attributes
+    if name == 'Age':
+        disabled = len(data['Edad'].dropna().unique()) == 0
+        selection = field.slider(
+            name,
+            value=(
+                int(min(data['Edad'].dropna().unique())) if not disabled else 0,
+                int(max(data['Edad'].dropna().unique())) if not disabled else 0
+            ),
+            disabled=disabled
+        )
+        data = data[(data['Edad'] >= selection[0]) & (data['Edad'] <= selection[1])]
+    else:
+        selection = field.multiselect(name, sorted(data[filter_name].unique()))
+        if selection:
+            data = data[data[filter_name].isin(selection)]
+
+    filters[filter_name] = selection
+
+total_filters = [str(_filter) for _filter in list(itertools.chain.from_iterable(filters.values()))]
+
+scale_type = option_menu(
+    menu_title=None,
+    options=[
+        "Regular Scales",
+        "JR Scales"
+    ],
+    icons=[
+        "align-end",
+        "align-middle"
+    ], # https://icons.getbootstrap.com/
+    orientation="horizontal",
+    styles={
+        "nav-link-selected": {"background-color": "#F78E1E"},
+    }
+)
+
+if scale_type == 'Regular Scales':
+    table = calculate_statistics_regular_scale(data)
+    st.dataframe(
+        table,
+        use_container_width=True,
+        column_config={
+            'variable': 'Variable',
+            'base': 'Base',
+            'mean': 'Mean',
+            'std': 'Standard Deviation',
+        }
+    )
+
+    st.download_button(
+        label="Download Data",
+        data=to_excel_bytes(table),
+        file_name=f'regular_{"_".join(total_filters)}.xlsx',
+        mime='application/xlsx'
+    )
+
+elif scale_type == 'JR Scales':
+    table = calculate_statistics_jr_scale(data)
+    st.dataframe(
+        table,
+        use_container_width=True,
+        column_config={
+            'variable': 'Variable',
+            'base': 'Base',
+            'mean': 'Mean',
+            'std': 'Standard Deviation',
+        }
+    )
+    st.download_button(
+        label="Download Data",
+        data=to_excel_bytes(table),
+        file_name=f'jr_{"_".join(total_filters)}.xlsx',
+        mime='application/xlsx'
+    )
