6db18beef31877206fd0b3b66232c2e045e8f3f7
Juan Avila <javilav@unal.edu.co>
2024-05-23 09:22:55 -0500

    Adds

--- modules/text_function.py
+++ modules/text_function.py
@@ -1,6 +1,10 @@
+from io import BytesIO
 import re
 import pyperclip
+import pyreadstat
 from unidecode import unidecode
+
+from modules.segment_spss import get_temp_file
 
 def questionFinder(txtC):
     questions=""
@@ -32,7 +36,7 @@ def questionFinder(txtC):
                         else:
                             questions+=word+" "
                     questions+="\n"
-                    pyperclip.copy(questions)
+    pyperclip.copy(questions)
     return questions
 
 def genRecodes(txtC):
@@ -45,7 +49,7 @@ def genRecodes(txtC):
             for word in spaces.split():
                 num=word
             recodes+="RECODE "+pref+num+"(1="+num+").\n"
-            pyperclip.copy(recodes)
+    pyperclip.copy(recodes)
     return recodes
 
 def genLabels(txtVars,txtOpt):
@@ -183,3 +187,121 @@ def categoryFinder(txtC):
         count+=1
     pyperclip.copy(result)
     return result
+
+def genRecodes2(txtC):
+    result=""
+    flag=True
+    vars=[]
+    num=""
+    for line in txtC.splitlines():
+        if flag:
+            if re.search("^[A-Z][1-90]",line):
+                vars.append(line)
+            else:
+                flag=False
+        else:
+            if re.search("[^\s]",line):
+                result=""
+    return result
+
+def genLabels2(txtC):
+    result=""
+    for line in txtC.splitlines():
+        result+=line.split()[0]+" \""+re.search("\s.*",line).group()[1:]+"\""+"\n"
+    return result
+
+def processSav(spss_file:BytesIO):
+    temp_file_name = get_temp_file(spss_file)
+    data, study_metadata = pyreadstat.read_sav(
+        temp_file_name,
+        apply_value_formats=False
+    )
+
+
+
+def processSavMulti(spss_file: BytesIO):
+    temp_file_name = get_temp_file(spss_file)
+    data, study_metadata = pyreadstat.read_sav(
+        temp_file_name,
+        apply_value_formats=False
+    )
+
+    columnsnames=study_metadata.column_names
+
+    count=0
+    for name in columnsnames:
+        finalname=re.sub("[.]","_",name)
+        if re.search("^[A-Z][1-90].*A",finalname):
+            finalname=re.sub("A","A_",finalname)
+        columnsnames[count]=finalname
+        count+=1
+    study_metadata.columns_names=columnsnames
+    recodes="* Encoding: UTF-8.\n\nDATASET ACTIVATE ConjuntoDatos1.\n"
+    for line in columnsnames:
+        if re.search("A_",line):
+            pref=re.search(".*_",line).group()
+            spaces=re.sub("_"," ",line)
+            num=""
+            for word in spaces.split():
+                num=word
+            recodes+="RECODE "+pref+num+"(1="+num+").\n"
+    recodes+="EXECUTE."
+    labels="* Encoding: UTF-8.\n"
+    serie=False
+    num=-21
+    prev=""
+    pref=""
+    optqueue=[]
+    options=study_metadata.value_labels
+    for line in columnsnames:
+        if re.search("A_",line):
+            if not serie:
+                serie=True
+                first=line
+                pref=re.search(".*_",line).group()
+                option=re.search("\'.*\'",str(options.get("labels"+str(num)))).group()[1:-1]
+                optqueue.append(option)
+                prev=line
+            else:
+                if pref==re.search(".*_",line).group():
+                    option=re.search("\'.*\'",str(options.get("labels"+str(num)))).group()[1:-1]
+                    optqueue.append(option)
+                    prev=line
+                else:
+                    labels+="VALUE LABELS "+first+" to "+ prev
+                    count=1
+                    for opt in optqueue:
+                        labels+="\n"+str(count)+" \""+opt+"\""
+                        count+=1
+                    labels+=".\n\n"
+                    optqueue=[]
+                    first=line
+                    pref=re.search(".*_",line).group()
+                    option=re.search("\'.*\'",str(options.get("labels"+str(num)))).group()[1:-1]
+                    optqueue.append(option)
+                    prev=line
+        num+=1
+    if serie:
+        labels+="VALUE LABELS "+first+" to "+ prev
+        count=1
+        for opt in optqueue:
+            labels+="\n"+str(count)+" \""+opt+"\""
+            count+=1
+        labels+=".\n\n"
+
+    varia=""
+    for name in columnsnames:
+        varia+=name+"\n"
+    pyperclip.copy(varia)
+    return recodes,labels,varia
+
+    #print("........................\n")
+    #print(study_metadata.value_labels)
+    #for i in range(10):
+    #    print(study_metadata.column_names[i])
+    #print(study_metadata.column_names)
+    #for i in range(10):
+    #    if "labels"+str(i) in study_metadata.value_labels:
+    #       st=str(study_metadata.value_labels.get("labels"+str(i)))
+    #        print (study_metadata.value_labels.get("labels"+str(i)))
+    #        print(re.search("\'.*\'",st[:-2]).group())
--- pages/03_Tools.py
+++ pages/03_Tools.py
@@ -1,13 +1,18 @@
 from PIL import Image
 import streamlit as st
+import pyreadstat
 
 from modules.styling import apply_default_style
 from modules.help import help_segment_spss
 from modules.transform_to_belcorp import transform_to_belcorp
 from modules.text_function import categoryFinder, questionFinder
 from modules.text_function import genRecodes
-from modules.text_function import genLabels
+from modules.text_function import genLabels2
 from modules.text_function import genIncludesList
+from modules.text_function import processSavMulti
+from modules.processor import processSav
+from modules.processor import getVarsSav
+from modules.processor import getCodeProcess
 
 # -------------- SETTINGS --------------
 page_title = "Tools"
@@ -37,31 +42,31 @@ with st.expander("Question Finder"):
         st.success("Copy to clipboard")
 
 
-with st.expander("Generate Recodes"):
-    entryText=st.text_area("Variables:",placeholder="Copy and paste the Vars column from the base")
-    btnFinder=st.button("Generate Recodes")
-    if btnFinder:
-        st.text_area("Recodes:",genRecodes(entryText))
-        st.success("Copy to clipboard")
-        st.download_button(
-            label="Download Sintaxis",
-            data=genRecodes(entryText),
-            file_name=f'Sintaxis1.sps'
-        )
+# with st.expander("Generate Recodes"):
+#     entryText=st.text_area("Variables:",placeholder="Copy and paste the Vars column from the base")
+#     btnFinder=st.button("Generate Recodes")
+#     if btnFinder:
+#         st.text_area("Recodes:",genRecodes(entryText))
+#         st.success("Copy to clipboard")
+#         st.download_button(
+#             label="Download Sintaxis",
+#             data=genRecodes(entryText),
+#             file_name=f'Sintaxis1.sps'
+#         )
 
 with st.expander("Generate Labels"):
     entryText=st.text_area("Variables:",placeholder="Copy and paste the Vars column from the base ")
     entryText2=st.text_area("Options:",placeholder="Copy and paste the Values options column from the base ")
     btnFinder=st.button("Generate Labels")
     if btnFinder:
-        st.text_area("Labels:",genLabels(entryText,entryText2))
+        st.text_area("Labels:",genLabels2(entryText))
         st.success("Copy to clipboard")
 
 
 with st.expander("Generate List of Includes"):
     entryText=st.text_area("Variables:",placeholder="Copy and paste the Vars from the GeneraAxis")
     entryText2=st.text_area("Nums:",placeholder="Copy and paste the num column from the includes Excel")
-    entryText3=st.text_area("Table:",placeholder="Copy and paste the Al table from the includes Excel")
+    entryText3=st.text_area("Table:",placeholder="Copy and paste the table from the includes Excel")
     btnFinder=st.button("Generate Includes List")
     if btnFinder:
         st.text_area("Labels:",genIncludesList(entryText,entryText2,entryText3))
@@ -73,3 +78,33 @@ with st.expander("Category Question Finder"):
     if btnFinder:
         st.text_area("Questions:",categoryFinder(entryText))
         st.success("Copy to clipboard")
+
+
+with st.expander("Tool Multiquestion"):
+    uploaded_file = st.file_uploader("Upload SAV file", type=["sav"])
+    if uploaded_file:
+        recodes,labels,variables=processSavMulti(uploaded_file)
+        st.text_area("RECODES:",recodes)
+        st.download_button(
+            label="Save Sintaxis",
+            data=recodes,
+            file_name='Sintaxis.sps',
+            mime='application/sps'
+        )
+        st.text_area("Labels:",labels)
+        st.download_button(
+            label="Save Etiquetas",
+            data=labels,
+            file_name='Etiquetas.txt'
+        )
+        st.text_area("Variables:",variables)
+        st.success("Vars name column copy to clipboard")
+
+with st.expander("Processor test"):
+    qtypes=st.text_area("Questions Types:")
+    uploaded_file = st.file_uploader("Upload SAV file ", type=["sav"])
+    if uploaded_file:
+        colVars=st.multiselect("Column Variables:",getVarsSav(uploaded_file))
+        proces=st.button("Process All")
+        if proces:
+            st.text_area("Commands",getCodeProcess(uploaded_file,colVars))
