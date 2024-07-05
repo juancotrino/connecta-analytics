8bea4494bb07e291bb5161def2c7dec3fe3aa364
Juan Cotrino <juan.cotrino@outlook.com>
2024-07-05 15:36:03 -0500

    Extended validation functions

--- app/pages/NOEL_Project_Registration.py
+++ app/pages/NOEL_Project_Registration.py
@@ -4,11 +4,14 @@ import pandas as pd
 
 import streamlit as st
 
-from app.modules.new_proejct_initialization import (
+from app.modules.noel_proejct_registration import (
     get_temp_file,
     read_sav_metadata,
     create_folder_structure,
-    get_projects_info
+    get_projects_info,
+    upload_file_to_sharepoint,
+    validate_data,
+    process_project
 )
 from app.modules.utils import get_countries
 
@@ -35,9 +38,13 @@ def main():
     in a SharePoint directory that will be used by the Data Science team.
     """)
 
-    st.header('Database')
+    st.header('Project files')
 
-    uploaded_file_sav = st.file_uploader("Upload `.sav` file", type=["sav"], key='noel_transform_sav')
+    uploaded_file_docx = st.file_uploader("Upload `.docx` questionnaire file", type=["docx"], key='noel_transform_docx')
+
+    uploaded_file_sav = st.file_uploader("Upload `.sav` database file", type=["sav"], key='noel_transform_sav')
+
+    sav_temp_file = get_temp_file(uploaded_file_sav)
 
     if uploaded_file_sav:
         temp_file_name = get_temp_file(uploaded_file_sav)
@@ -53,8 +60,6 @@ def main():
 
     with st.form('noel_project'):
 
-
-
         col1, col2, col3 = st.columns(3)
 
         with col1:
@@ -66,8 +71,8 @@ def main():
                     project_id = None
                     st.warning('Project ID should be a number.')
 
-            categories = st.selectbox(
-                'Categories',
+            category = st.selectbox(
+                'Category',
                 options=categories,
                 index=None,
                 placeholder="Select category..."
@@ -81,8 +86,8 @@ def main():
                 placeholder="Select country..."
             )
 
-            categories = st.selectbox(
-                'Subcategories',
+            sub_category = st.selectbox(
+                'Subcategory',
                 options=sub_categories,
                 index=None,
                 placeholder="Select subcategory..."
@@ -92,8 +97,8 @@ def main():
             project_name = st.text_input('Project name')
             project_name = project_name.strip().lower().replace(' ', '_')
 
-            clients = st.selectbox(
-                'Subcategories',
+            client = st.selectbox(
+                'Client',
                 options=clients,
                 index=None,
                 placeholder="Select client..."
@@ -110,7 +115,22 @@ def main():
             use_container_width=True,
             key="variables_mapping_df",
             column_config=config
-        )
+        ).dropna(subset='variable_in_db')
+
+        variables_mapping_dict = pd.Series(
+            variables_mapping['variable_in_db'].values,
+            index=variables_mapping['variable_name']
+        ).to_dict()
+
+        project_info = {
+            'project_id': int(project_id),
+            'category': category,
+            'country': country,
+            'sub_category': sub_category,
+            'project_name': project_name,
+            'client': client,
+            'variables_mapping': variables_mapping_dict
+        }
 
         if country:
             country_code = countries_iso_2_code[country].lower()
@@ -121,13 +141,41 @@ def main():
 
         if project_id and country and project_name:
             if create_button:
-                base_path = f'Documentos compartidos/estudios_externos/{id_project_name}'
                 try:
-                    with st.spinner('Processing...'):
-                        create_folder_structure(base_path)
-                        # folder_url = f'https://connectasas.sharepoint.com/sites/connecta-ciencia_de_datos/Documentos%20compartidos/estudios_externos/{id_project_name}'
-                        # st.success(
-                        #     f'Study root folder created successfully. Visit the new folder [here]({folder_url}).'
-                        # )
+                    validate_data(variables_mapping_dict, metadata_df)
+
+                    base_path = f'Documentos compartidos/estudios_externos/{id_project_name}'
+                    if uploaded_file_docx and uploaded_file_sav:
+                        try:
+                            with st.spinner('Creating folder structure...'):
+                                create_folder_structure(base_path)
+                                folder_url = f'https://connectasas.sharepoint.com/sites/connecta-ciencia_de_datos/Documentos%20compartidos/estudios_externos/{id_project_name}'
+                                st.success(
+                                    f'Study root folder created successfully. Visit the new folder [here]({folder_url}).'
+                                )
+
+                            with st.spinner('Uploading questionnaire and database to Sharepoint...'):
+                                upload_file_to_sharepoint(base_path, uploaded_file_docx, 'questionnaire.docx')
+                                upload_file_to_sharepoint(base_path, uploaded_file_sav, 'db.sav')
+                                st.success(
+                                    f'Questionnaire and database uploaded successfully into above created folder.'
+                                )
+
+                            with st.spinner('Processing database...'):
+                                process_project(sav_temp_file)
+                                pass
+                                # upload_file_to_sharepoint(uploaded_file_sav)
+                                # st.success(
+                                #     f'Study root folder created successfully. Visit the new folder [here]({folder_url}).'
+                                # )
+
+                        except Exception as e:
+                            st.error(e)
+
+                    else:
+                        st.error(
+                            'A questionnaire in `.docx` format and database in `.sav` format must be attached into the respective fields.'
+                        )
+
                 except Exception as e:
                     st.error(e)
