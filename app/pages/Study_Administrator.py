31688539c76e4186a37fed4ea55323c8416abf02
Juan Cotrino <juan.cotrino@outlook.com>
2024-08-23 15:49:34 -0500

    Changed retrieval of study data for file uploader

--- app/pages/Study_Administrator.py
+++ app/pages/Study_Administrator.py
@@ -283,59 +283,62 @@ def main():
     with tab4:
 
         study_id = st.selectbox('Study ID', options=reversed(sudies_ids), index=None, placeholder='Select study ID', key='study_id_file_uploader')
-        studies_countries = sudies_ids_country[sudies_ids_country['study_id'] == study_id]['country'].sort_values().unique()
+        if study_id:
+            study_data = get_study_data(study_id)
+            studies_countries = study_data[study_data['study_id'] == study_id]['country'].sort_values().unique()
+            # studies_countries = sudies_ids_country[sudies_ids_country['study_id'] == study_id]['country'].sort_values().unique()
 
-        if len(studies_countries) > 1:
-            country = st.selectbox('Country', options=studies_countries, index=None, placeholder='Select study country', key='country_file_uploader')
-        elif len(studies_countries) == 1:
-            country = st.text_input('Country', studies_countries[0], disabled=True, key='country_file_uploader')
-        else:
-            country = None
-
-        specific_studies = sudies_ids_country[
-            (sudies_ids_country['study_id'] == study_id) &
-            (sudies_ids_country['country'] == country)
-        ]['study_name'].sort_values().reset_index(drop=True)
-
-        if study_id and country:
-            if len(list(set(specific_studies))) > 1:
-                specific_study = st.radio('Select study:', options=specific_studies, index=None)
+            if len(studies_countries) > 1:
+                country = st.selectbox('Country', options=studies_countries, index=None, placeholder='Select study country', key='country_file_uploader')
+            elif len(studies_countries) == 1:
+                country = st.text_input('Country', studies_countries[0], disabled=True, key='country_file_uploader')
             else:
-                specific_study = specific_studies[0]
+                country = None
 
-        if study_id and country and specific_study:
-            country_code = countries_codes[country]
-            id_study_name = f"{study_id}_{country_code.lower()}_{specific_study.replace(' ', '_').lower()}"
-            base_path = f'Documentos compartidos/estudios/{id_study_name}'
-            folder_url = f'https://connectasas.sharepoint.com/sites/connecta-ciencia_de_datos/Documentos%20compartidos/estudios/{id_study_name}'
+            specific_studies = sudies_ids_country[
+                (sudies_ids_country['study_id'] == study_id) &
+                (sudies_ids_country['country'] == country)
+            ]['study_name'].sort_values().reset_index(drop=True)
 
-            st.info(f'Upload to `{specific_study}` study for country: {country}')
+            if study_id and country:
+                if len(list(set(specific_studies))) > 1:
+                    specific_study = st.radio('Select study:', options=specific_studies, index=None)
+                else:
+                    specific_study = specific_studies[0]
 
-            files_info = get_upload_files_info()
+            if study_id and country and specific_study:
+                country_code = countries_codes[country]
+                id_study_name = f"{study_id}_{country_code.lower()}_{specific_study.replace(' ', '_').lower()}"
+                base_path = f'Documentos compartidos/estudios/{id_study_name}'
+                folder_url = f'https://connectasas.sharepoint.com/sites/connecta-ciencia_de_datos/Documentos%20compartidos/estudios/{id_study_name}'
 
-            for title, file_info in files_info.items():
-                st.subheader(title.replace('_', ' ').capitalize())
+                st.info(f'Upload to `{specific_study}` study for country: {country}')
 
-                with st.form(f'upload_{title}_form'):
-                    uploaded_file = st.file_uploader(
-                        f"Upload `.{file_info['file_type']}` {title.replace('_', ' ')} file",
-                        type=[file_info['file_type']],
-                        key=f"{title.replace('_', ' ')}_{file_info['file_type']}"
-                    )
-                    upload_file = st.form_submit_button(f"Upload {title.replace('_', ' ')}")
+                files_info = get_upload_files_info()
 
-                if uploaded_file and upload_file:
-                    file_path = file_info['path']
-                    files = get_last_file_version_in_sharepoint(id_study_name, 'estudios', file_path)
-                    files = [file for file in files if file_info['acronym'] in file]
-                    if not files:
-                        file_name = f"{id_study_name}_{file_info['acronym']}_V1.{file_info['file_type']}"
-                    else:
-                        last_version_number = max(int(file.split('_')[-1].split('.')[0].replace('V', '')) for file in files)
-                        file_name = f"{id_study_name}_{file_info['acronym']}_V{last_version_number + 1}.{file_info['file_type']}"
+                for title, file_info in files_info.items():
+                    st.subheader(title.replace('_', ' ').capitalize())
 
-                    with st.spinner(f"Uploading {title.replace('_', ' ')} to Sharepoint..."):
-                        upload_file_to_sharepoint(f'{base_path}/{file_path}', uploaded_file, file_name)
-                        st.success(
-                            f"{title.replace('_', ' ').capitalize()} uploaded successfully into [study's folder]({folder_url}/{file_path})."
+                    with st.form(f'upload_{title}_form'):
+                        uploaded_file = st.file_uploader(
+                            f"Upload `.{file_info['file_type']}` {title.replace('_', ' ')} file",
+                            type=[file_info['file_type']],
+                            key=f"{title.replace('_', ' ')}_{file_info['file_type']}"
                         )
+                        upload_file = st.form_submit_button(f"Upload {title.replace('_', ' ')}")
+
+                    if uploaded_file and upload_file:
+                        file_path = file_info['path']
+                        files = get_last_file_version_in_sharepoint(id_study_name, 'estudios', file_path)
+                        files = [file for file in files if file_info['acronym'] in file]
+                        if not files:
+                            file_name = f"{id_study_name}_{file_info['acronym']}_V1.{file_info['file_type']}"
+                        else:
+                            last_version_number = max(int(file.split('_')[-1].split('.')[0].replace('V', '')) for file in files)
+                            file_name = f"{id_study_name}_{file_info['acronym']}_V{last_version_number + 1}.{file_info['file_type']}"
+
+                        with st.spinner(f"Uploading {title.replace('_', ' ')} to Sharepoint..."):
+                            upload_file_to_sharepoint(f'{base_path}/{file_path}', uploaded_file, file_name)
+                            st.success(
+                                f"{title.replace('_', ' ').capitalize()} uploaded successfully into [study's folder]({folder_url}/{file_path})."
+                            )
