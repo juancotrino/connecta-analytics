f6176a5d1cf07765889eba397f556730b89e7edf
Juan Cotrino <juan.cotrino@outlook.com>
2024-09-06 11:37:11 -0500

    First checks for accessible files in file uploader

--- app/pages/Study_Administrator.py
+++ app/pages/Study_Administrator.py
@@ -364,94 +364,97 @@ def main():
 
     with tab4:
 
-        study_id = st.selectbox('Study ID', options=reversed(sudies_ids), index=None, placeholder='Select study ID', key='study_id_file_uploader')
-        if study_id:
-            study_data = get_study_data(study_id)
-            studies_countries = study_data[study_data['study_id'] == study_id]['country'].sort_values().unique()
+        files_info = get_upload_files_info()
+        accessible_files = filter_files_by_roles(files_info, st.session_state['roles'])
 
-            if len(studies_countries) > 1:
-                country = st.selectbox('Country', options=studies_countries, index=None, placeholder='Select study country', key='country_file_uploader')
-            elif len(studies_countries) == 1:
-                country = st.text_input('Country', studies_countries[0], disabled=True, key='country_file_uploader')
-            else:
-                country = None
+        if not accessible_files:
+            st.warning('You have no permissions to upload files.')
+        else:
+            study_id = st.selectbox('Study ID', options=reversed(sudies_ids), index=None, placeholder='Select study ID', key='study_id_file_uploader')
+            if study_id:
+                study_data = get_study_data(study_id)
+                studies_countries = study_data[study_data['study_id'] == study_id]['country'].sort_values().unique()
 
-            specific_studies = sudies_ids_country[
-                (sudies_ids_country['study_id'] == study_id) &
-                (sudies_ids_country['country'] == country)
-            ]['study_name'].sort_values().reset_index(drop=True)
-
-            if study_id and country:
-                if len(list(set(specific_studies))) > 1:
-                    specific_study = st.radio('Select study:', options=specific_studies, index=None)
+                if len(studies_countries) > 1:
+                    country = st.selectbox('Country', options=studies_countries, index=None, placeholder='Select study country', key='country_file_uploader')
+                elif len(studies_countries) == 1:
+                    country = st.text_input('Country', studies_countries[0], disabled=True, key='country_file_uploader')
                 else:
-                    specific_study = specific_studies[0]
+                    country = None
 
-            if study_id and country and specific_study:
-                country_code = countries_iso_2_code[country]
-                id_study_name = f"{study_id}_{country_code.lower()}_{specific_study.replace(' ', '_').lower()}"
-                base_path = f'Documentos compartidos/estudios/{id_study_name}'
-                folder_url = f'https://connectasas.sharepoint.com/sites/connecta-ciencia_de_datos/Documentos%20compartidos/estudios/{id_study_name}'
+                specific_studies = sudies_ids_country[
+                    (sudies_ids_country['study_id'] == study_id) &
+                    (sudies_ids_country['country'] == country)
+                ]['study_name'].sort_values().reset_index(drop=True)
 
-                st.info(f'Upload to `{specific_study}` study for country: {country}')
-
-                files_info = get_upload_files_info()
-                accessible_files = filter_files_by_roles(files_info, st.session_state['roles'])
-
-                title = st.selectbox('File to upload', options=sorted([file.replace('_', ' ').capitalize() for file in accessible_files.keys()]))
-                file_info = accessible_files[title.replace(' ', '_').lower()]
-
-                with st.form(f'upload_{title}_form'):
-
-                    if file_info['acronym'] and file_info['file_type']:
-                        uploaded_file = st.file_uploader(
-                            f"Upload `.{'` or `.'.join(file_info['file_type'].split(','))}` {title.replace('_', ' ')} file",
-                            type=file_info['file_type'].split(','),
-                            key=f"{title.replace('_', ' ')}_{file_info['file_type']}"
-                        )
-                        upload_file = st.form_submit_button(f"Upload {title.replace('_', ' ')}")
+                if study_id and country:
+                    if len(list(set(specific_studies))) > 1:
+                        specific_study = st.radio('Select study:', options=specific_studies, index=None)
                     else:
-                        uploaded_file = st.file_uploader(
-                            f"Upload {title.replace('_', ' ')} file",
-                            type=None,
-                            key=f"{title.replace('_', ' ')}"
-                        )
-                        upload_file = st.form_submit_button(f"Upload {title.replace('_', ' ')}")
+                        specific_study = specific_studies[0]
 
-                if uploaded_file and upload_file:
-                    file_path = file_info['path']
-                    file_type = uploaded_file.name.split('.')[-1]
-                    if file_info['acronym'] and file_info['file_type']:
-                        files = get_last_file_version_in_sharepoint(id_study_name, 'estudios', file_path)
-                        files = [file for file in files if file_info['acronym'] in file]
-                        if not files:
-                            file_name = f"{id_study_name}_{file_info['acronym']}_V1.{file_type}"
-                        else:
-                            last_version_number = max(int(file.split('_')[-1].split('.')[0].replace('V', '')) for file in files)
-                            file_name = f"{id_study_name}_{file_info['acronym']}_V{last_version_number + 1}.{file_type}"
-                    else:
-                        file_name = uploaded_file.name
+                if study_id and country and specific_study:
+                    country_code = countries_iso_2_code[country]
+                    id_study_name = f"{study_id}_{country_code.lower()}_{specific_study.replace(' ', '_').lower()}"
+                    base_path = f'Documentos compartidos/estudios/{id_study_name}'
+                    folder_url = f'https://connectasas.sharepoint.com/sites/connecta-ciencia_de_datos/Documentos%20compartidos/estudios/{id_study_name}'
 
-                    with st.spinner(f"Uploading {title.replace('_', ' ')} to Sharepoint..."):
-                        try:
-                            upload_file_to_sharepoint(f'{base_path}/{file_path}', uploaded_file, file_name)
-                            st.success(
-                                f"{title.replace('_', ' ').capitalize()} uploaded successfully into [study's folder]({folder_url}/{file_path})."
+                    st.info(f'Upload to `{specific_study}` study for country: {country}')
+
+                    title = st.selectbox('File to upload', options=sorted([file.replace('_', ' ').capitalize() for file in accessible_files.keys()]))
+                    file_info = accessible_files[title.replace(' ', '_').lower()]
+
+                    with st.form(f'upload_{title}_form'):
+
+                        if file_info['acronym'] and file_info['file_type']:
+                            uploaded_file = st.file_uploader(
+                                f"Upload `.{'` or `.'.join(file_info['file_type'].split(','))}` {title.replace('_', ' ')} file",
+                                type=file_info['file_type'].split(','),
+                                key=f"{title.replace('_', ' ')}_{file_info['file_type']}"
                             )
+                            upload_file = st.form_submit_button(f"Upload {title.replace('_', ' ')}")
+                        else:
+                            uploaded_file = st.file_uploader(
+                                f"Upload {title.replace('_', ' ')} file",
+                                type=None,
+                                key=f"{title.replace('_', ' ')}"
+                            )
+                            upload_file = st.form_submit_button(f"Upload {title.replace('_', ' ')}")
+
+                    if uploaded_file and upload_file:
+                        file_path = file_info['path']
+                        file_type = uploaded_file.name.split('.')[-1]
+                        if file_info['acronym'] and file_info['file_type']:
+                            files = get_last_file_version_in_sharepoint(id_study_name, 'estudios', file_path)
+                            files = [file for file in files if file_info['acronym'] in file]
+                            if not files:
+                                file_name = f"{id_study_name}_{file_info['acronym']}_V1.{file_type}"
+                            else:
+                                last_version_number = max(int(file.split('_')[-1].split('.')[0].replace('V', '')) for file in files)
+                                file_name = f"{id_study_name}_{file_info['acronym']}_V{last_version_number + 1}.{file_type}"
+                        else:
+                            file_name = uploaded_file.name
+
+                        with st.spinner(f"Uploading {title.replace('_', ' ')} to Sharepoint..."):
+                            try:
+                                upload_file_to_sharepoint(f'{base_path}/{file_path}', uploaded_file, file_name)
+                                st.success(
+                                    f"{title.replace('_', ' ').capitalize()} uploaded successfully into [study's folder]({folder_url}/{file_path})."
+                                )
+                            except Exception as e:
+                                st.error('There is no folder for this study in SharePoint.')
+
+                        ms_teams_file_name = title.replace(' ', '_').lower()
+                        ms_teams_study_info = {
+                            'study_id': study_id,
+                            'study_name': specific_study,
+                            'country': country,
+                            'file_name': file_name,
+                            'author': st.session_state['name'],
+                            'file_folder': f'{folder_url}/{file_path}'
+                        }
+
+                        try:
+                            msteams_card_file_update(ms_teams_file_name, ms_teams_study_info)
                         except Exception as e:
                             st.error('There is no folder for this study in SharePoint.')
-
-                    ms_teams_file_name = title.replace(' ', '_').lower()
-                    ms_teams_study_info = {
-                        'study_id': study_id,
-                        'study_name': specific_study,
-                        'country': country,
-                        'file_name': file_name,
-                        'author': st.session_state['name'],
-                        'file_folder': f'{folder_url}/{file_path}'
-                    }
-
-                    try:
-                        msteams_card_file_update(ms_teams_file_name, ms_teams_study_info)
-                    except Exception as e:
-                        st.error('There is no folder for this study in SharePoint.')
